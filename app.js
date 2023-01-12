b707f034afcfc95748aa3599883e47ff2c72a8f4
unknown <chinomsojohnson@gmail.com>
2023-01-12 22:58:51 +0100

    changes

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+
+const handleLinkResolver  = (doc) => {
+  return '/'
+}
+
+
+const initApi = ({req}) => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    req: req,
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN
+  })
+}
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver
+  }
+
+  res.locals.PrismicDOM = PrismicDOM
+
+  next()
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
@@ -15,7 +42,7 @@ app.get('/about', (req, res) => {
 })
 
   
-app.get('/detail/:id', (req, res) => {
+app.get('/detail/:uid', (req, res) => {
     res.render('pages/detail')
 })
 
